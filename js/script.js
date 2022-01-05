88e4d0066919c54c4d07ed50cc422f3b349d9fa5
Justus Siebert <info@justus-siebert.de>
2022-01-05 18:23:52 +0100

    js: rendered and filtered todo-list works!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const listTodos = document.querySelector("#listToDos");
 const buttonAddToDo = document.querySelector("#buttonAddTodo");
 const inputFieldAddToDo = document.querySelector("#inputFieldAddTodo");
+//const radioChangeFilter = document.querySelector('[name="filter"]:checked').value;
+let radioFilter = document.querySelectorAll('input[name="filter"]');
 
 const stateInit = {
   filter: "alle",
@@ -16,15 +18,42 @@ const toDos = [
 function addTodo() {
   const ff = "";
 }
+
+function updateFilter(filter) {
+  stateInit.filter = filter;
+
+  renderListTodos(filter);
+  //console.log("Filter aktuell: " + stateInit.filter);
+}
+
 function updateTodo() {}
 
 //function removeTodo() {}
 
+let filteredToDoList;
+
 function renderListTodos(filter) {
-  for (let i = 0; i < toDos.length; i++) {
-    console.log("Text: " + toDos[i].todoText + "Done: " + toDos[i].done);
+  let filterIsDone = false;
+  if (filter === "alle") {
+    filteredToDoList = toDos;
+  } else {
+    if (filter === "erledigt") {
+      filterIsDone = true;
+    }
+    //alert("Filter: " + filter);
+    filteredToDoList = toDos.filter(
+      (isDoneOrNot) => isDoneOrNot.done === filterIsDone
+    );
+  }
+
+  listTodos.innerHTML = "";
+
+  //for (let i = 0; i < toDos.length; i++) {
+  for (let i = 0; i < filteredToDoList.length; i++) {
+    //console.log("Text: " + toDos[i].todoText + "Done: " + toDos[i].done);
     let textDone = " checked";
-    if (!toDos[i].done === true) {
+    //if (!toDos[i].done === true) {
+    if (!filteredToDoList[i].done === true) {
       textDone = "";
     }
     let newContainerTodo = document.createElement("li");
@@ -37,7 +66,8 @@ function renderListTodos(filter) {
       ' /><label for="todo-' +
       i +
       '">' +
-      toDos[i].todoText +
+      //toDos[i].todoText +
+      filteredToDoList[i].todoText +
       "</label>";
     //let newContentTodo = document.createTextNode("hhh");
     //newContainerTodo.appendChild(newContentTodo);
@@ -53,7 +83,31 @@ buttonAddToDo.addEventListener("click", function (e) {
   addTodo();
 });
 
+for (let i = 0; i < radioFilter.length; i++) {
+  radioFilter[i].addEventListener("change", function () {
+    let val = this.value; // this == the clicked radio,
+    //console.log("Wert: " + val);
+    updateFilter(val);
+  });
+}
+/*
+radioChangeFilter.addEventListener("change", function (e) {
+  updateFilter();
+});
+*/
+
+function initState() {
+  for (let i = 0; i < radioFilter.length; i++) {
+    if (stateInit.filter === radioFilter[i].value) {
+      radioFilter[i].checked = true;
+      //alert("Filter: " + stateInit.filter);
+      //stateInit.filter = radioFilter[i].value;
+    }
+  }
+}
+
 window.onload = function () {
   //alert("ccc");
-  renderListTodos();
+  initState();
+  renderListTodos(stateInit.filter);
 };
