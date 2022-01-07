b0076c3de843d7f2dfa08e95dd550f991b9a002c
Justus Siebert <info@justus-siebert.de>
2022-01-07 12:38:20 +0100

    js: first refacturing, some out-commented code from yesterday deleted, and rubbish-comments. some make-sense-comments added. Still more refacture-work ahead

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ const listTodos = document.querySelector("#listToDos");
 const buttonRemoveToDo = document.querySelector("#buttonDeleteDoneTodos");
 const buttonAddToDo = document.querySelector("#buttonAddTodo");
 const inputFieldAddToDo = document.querySelector("#inputFieldAddTodo");
-//const radioChangeFilter = document.querySelector('[name="filter"]:checked').value;
 let radioFilter = document.querySelectorAll('input[name="filter"]');
+
+let cleanedToDoList = [];
+let filteredToDoList;
 
 const stateInit = {
   filter: "alle",
-  numberToDos: 0,
 };
 /*
 const toDos = [
@@ -35,95 +36,62 @@ function addTodo() {
 
 function updateFilter(filter) {
   stateInit.filter = filter;
-
   renderListTodos(filter);
-  //console.log("Filter aktuell: " + stateInit.filter);
 }
 
 function updateTodo() {
   const listItems = listTodos.getElementsByTagName("li");
-  //const listItems = listTodos.getElementsByTagName("input");
-  //alert(listItems.length);
   for (let i = 0; i < listItems.length; i++) {
     let checkBox = document.getElementById("todo-" + i);
-
-    //checkBox.addEventListener("change", function (e) {
     checkBox.addEventListener("change", function (e) {
-      //alert(checkBox.id);
       let isDone = true;
-      if (checkBox.checked === true) {
-        //alert("Item: checked");
-      } else {
-        //alert("nicht checked");
+      if (checkBox.checked !== true) {
         isDone = false;
       }
       toDos[i].done = isDone;
     });
-    /*
-    
-*/
-    //alert(listItems[i].value);
   }
 }
 
+// wenn in der Liste was (checkbox) geklickt wird, also done-status augedated wird...
 listTodos.addEventListener("input", function (e) {
-  //alert("geklickt");
   updateTodo();
   renderListTodos(stateInit.filter);
 });
 
-let cleanedToDoList = [];
-
-function removeTodo(filterKey, filterValue) {
-  //let toDosToBeRemovedTmp = [];
-  for (let i = 0; i < toDos.length; i++) {
-    //if (toDos[i].filterKey === toDos[i].filterValue) {
-    if (toDos[i].done === true) {
-      //toDos[i] = "";
-      //delete toDos[i];
-    }
-    //alert("Key: " + toDos[i].done);
-  }
+function removeTodo() {
   cleanedToDoList = toDos.filter((isDoneOrNot) => isDoneOrNot.done === false);
   toDos = cleanedToDoList;
-  //toDos = [];
-  console.log("Bereinigte: " + cleanedToDoList);
-  console.log("neue Liste: " + toDos);
 }
 
 buttonRemoveToDo.addEventListener("click", function (e) {
-  removeTodo("done", "true");
+  removeTodo();
   renderListTodos(stateInit.filter);
 });
 
-let filteredToDoList;
-
+// render fresh todo-list in dom
 function renderListTodos(filter) {
+  listTodos.innerHTML = "";
   let filterIsDone = false;
+
   if (filter === "alle") {
     filteredToDoList = toDos;
   } else {
     if (filter === "erledigt") {
       filterIsDone = true;
     }
-    //alert("Filter: " + filter);
+
     filteredToDoList = toDos.filter(
       (isDoneOrNot) => isDoneOrNot.done === filterIsDone
     );
   }
 
-  listTodos.innerHTML = "";
-
-  //for (let i = 0; i < toDos.length; i++) {
   for (let i = 0; i < filteredToDoList.length; i++) {
-    //console.log("Text: " + toDos[i].todoText + "Done: " + toDos[i].done);
     let textDone = " checked";
-    //if (!toDos[i].done === true) {
     if (!filteredToDoList[i].done === true) {
       textDone = "";
     }
     let newContainerTodo = document.createElement("li");
-    //let newContentTodo = document.createTextNode(toDos[i].todoText);
     newContainerTodo.innerHTML =
       '<input type="checkbox" id="todo-' +
       i +
@@ -132,15 +100,8 @@ function renderListTodos(filter) {
       ' /><label for="todo-' +
       i +
       '">' +
-      //toDos[i].todoText +
       filteredToDoList[i].todoText +
       "</label>";
-    //let newContentTodo = document.createTextNode("hhh");
-    //newContainerTodo.appendChild(newContentTodo);
-
-    //let currentDiv = document.getElementById("listToDos");
-    //document.body.insertBefore(newContainerTodo, listTodos);
-    //document.getElementById("listToDos").appendChild(newContainerTodo);
     listTodos.appendChild(newContainerTodo);
   }
 }
@@ -149,44 +110,26 @@ buttonAddToDo.addEventListener("click", function (e) {
   addTodo();
 });
 
+// den Filter (radio-buttons) im dom durchgehen, wenn da geklickt wird, state aktualisieren
 for (let i = 0; i < radioFilter.length; i++) {
   radioFilter[i].addEventListener("change", function () {
     let val = this.value; // this == the clicked radio,
-    //console.log("Wert: " + val);
     updateFilter(val);
   });
 }
-/*
-radioChangeFilter.addEventListener("change", function (e) {
-  updateFilter();
-});
-*/
 
+// bei start der Seite: alles frisch aus state holen, also im dom den filter (radio) checken, der laut state gecheckt sein soll
 function initState() {
   for (let i = 0; i < radioFilter.length; i++) {
     if (stateInit.filter === radioFilter[i].value) {
       radioFilter[i].checked = true;
-      //alert("Filter: " + stateInit.filter);
-      //stateInit.filter = radioFilter[i].value;
     }
   }
 }
 
+// bei Laden der Seite: frischen state holen (welcher filter status gesetzt sein soll, radio)
+// Liste wird auch neu gerendert, gibt allerdings anfangs keine, könnte aber ja sein, wenn man was in den state bw. todo-array rein schreibt
 window.onload = function () {
-  //alert("ccc");
   initState();
   renderListTodos(stateInit.filter);
-  /*
-  const checkboxesOfListTodos = document.querySelectorAll(
-    "#listToDos > li > input[type=checkbox]"
-  );
-  alert(checkboxesOfListTodos.length);
-  */
 };
-/*
-function initAll() {
-  //alert("h");
-  //initAll(), setTimeout(1000);
-}
-initAll(), setTimeout(100);
-*/
